254ca2ecfe41847fe903bc7318f0a73b855b9daf
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2024-01-22 19:44:12 -0500

    Import relevant files from REV MAXSwerve Java template
    From: https://github.com/REVrobotics/MAXSwerve-Java-Template

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,38 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
+  // Subsystems
+  private final DriveSubsystem drive = new DriveSubsystem();
+
+  // Controllers
+  XboxController driveController = new XboxController(Constants.OIConstants.kDriverControllerPort);
+
   public RobotContainer() {
     configureBindings();
+
+    drive.setDefaultCommand(
+        // The left stick controls translation of the robot.
+        // Turning is controlled by the X axis of the right stick.
+        new RunCommand(
+            () -> drive.drive(
+                -MathUtil.applyDeadband(driveController.getLeftY(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(driveController.getLeftX(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(driveController.getRightX(), OIConstants.kDriveDeadband),
+                true, true),
+            drive));
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
