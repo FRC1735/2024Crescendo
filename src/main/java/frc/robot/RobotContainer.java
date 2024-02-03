0d3eb98f4a1f87ef57149ba286ee40dde8917309
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2024-02-03 09:47:39 -0500

    Minimal code to drive YAGSL subsystem
    No idea if this will work, tried to get the most basic things going based on the example.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,36 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.SwerveSubsystem;
 
 public class RobotContainer {
+  // Subsystems
+  private final SwerveSubsystem drive = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+
+  // Controllers
+  XboxController driverController = new XboxController(0);
+
   public RobotContainer() {
     configureBindings();
+
+    Command driveFieldOrientedAnglularVelocity = drive.driveCommand(
+        () -> MathUtil.applyDeadband(driverController.getLeftY(), OperatorConstants.LEFT_Y_DEADBAND),
+        () -> MathUtil.applyDeadband(driverController.getLeftX(), OperatorConstants.LEFT_X_DEADBAND),
+        () -> driverController.getRawAxis(2));
+
+    drive.setDefaultCommand(driveFieldOrientedAnglularVelocity);
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,59 @@
+package frc.robot.subsystems;
+
+import java.io.File;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+
+// Based on: https://github.com/BroncBotz3481/YAGSL-Example/blob/main/src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+// Notice all the telemetry, pathplanner, etc. We should do that once things are running at a basic level.
+
+public class SwerveSubsystem extends SubsystemBase {
+    private final SwerveDrive swerveDrive;
+    public double maximumSpeed = Units.feetToMeters(14.5);
+
+    public SwerveSubsystem(File directory) {
+        try {
+            swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier headingX,
+            DoubleSupplier headingY) {
+        return run(() -> {
+            double xInput = Math.pow(translationX.getAsDouble(), 3); // Smooth controll out
+            double yInput = Math.pow(translationY.getAsDouble(), 3); // Smooth controll out
+            // Make the robot move
+            driveFieldOriented(swerveDrive.swerveController.getTargetSpeeds(xInput, yInput,
+                    headingX.getAsDouble(),
+                    headingY.getAsDouble(),
+                    swerveDrive.getOdometryHeading().getRadians(),
+                    swerveDrive.getMaximumVelocity()));
+        });
+    }
+
+    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY,
+            DoubleSupplier angularRotationX) {
+        return run(() -> {
+            // Make the robot move
+            swerveDrive.drive(
+                    new Translation2d(Math.pow(translationX.getAsDouble(), 3) * swerveDrive.getMaximumVelocity(),
+                            Math.pow(translationY.getAsDouble(), 3) * swerveDrive.getMaximumVelocity()),
+                    Math.pow(angularRotationX.getAsDouble(), 3) * swerveDrive.getMaximumAngularVelocity(),
+                    true,
+                    false);
+        });
+    }
+
+    public void driveFieldOriented(ChassisSpeeds velocity) {
+        swerveDrive.driveFieldOriented(velocity);
+    }
+}
