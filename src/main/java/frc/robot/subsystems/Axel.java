a466d033aaab3f48b5ed1659413677bf1ec8e577
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2024-02-08 19:39:36 -0500

    Create Axel.java
    Create skeleton axel subsystem.

---
+++ src/main/java/frc/robot/subsystems/Axel.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Axel extends SubsystemBase {
+  private final CANSparkMax leftMotor = new CANSparkMax(0,MotorType.kBrushless);
+  private final CANSparkMax rightMotor = new CANSparkMax(0,MotorType.kBrushless);
+  /** Creates a new Axel. */
+  public Axel() {}
+
+ public void pickUp() {
+
+ };
+
+
+ public void speaker() {
+
+ };
+
+public void amp() {
+
+
+};
+  
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
