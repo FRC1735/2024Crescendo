90b2ae81e23c00f70037eeed4717184849b963af
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2024-02-17 10:24:03 -0500

    Updatd and switched to 2024 swerve config

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,8 +18,7 @@ import frc.robot.subsystems.SwerveSubsystem;
 
 public class RobotContainer {
   // Subsystems
-  // private final SwerveSubsystem drive = new SwerveSubsystem(new
-  // File(Filesystem.getDeployDirectory(), "swerve-2023"));
+  private final SwerveSubsystem drive = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve-2024"));
   private final Collector collector = new Collector();
   private final Shooter shooter = new Shooter();
   private final Axel axel = new Axel();
@@ -30,39 +29,37 @@ public class RobotContainer {
   public RobotContainer() {
     configureBindings();
 
-    /*
-     * Command driveFieldOrientedDirectAngle = drive.driveCommand(
-     * () -> MathUtil.applyDeadband(driverController.getLeftY(),
-     * OperatorConstants.LEFT_Y_DEADBAND),
-     * () -> MathUtil.applyDeadband(driverController.getLeftX(),
-     * OperatorConstants.LEFT_X_DEADBAND),
-     * () -> {
-     * double rightX = driverController.getRightX();
-     * // System.out.println("RightX: " + rightX);
-     * return rightX;
-     * },
-     * () -> {
-     * double rightY = driverController.getRightY();
-     * // System.out.println("RightY: " + rightY);
-     * return rightY;
-     * });
-     * 
-     * Command driveFieldOrientedAnglularVelocity = drive.driveCommand(
-     * () -> MathUtil.applyDeadband(driverController.getLeftY(),
-     * OperatorConstants.LEFT_Y_DEADBAND),
-     * () -> MathUtil.applyDeadband(driverController.getLeftX(),
-     * OperatorConstants.LEFT_X_DEADBAND),
-     * () -> driverController.getRawAxis(2));
-     * 
-     * // drive.setDefaultCommand(driveFieldOrientedDirectAngle);
-     * drive.setDefaultCommand(driveFieldOrientedAnglularVelocity);
-     */
+    Command driveFieldOrientedDirectAngle = drive.driveCommand(
+        () -> MathUtil.applyDeadband(driverController.getLeftY(),
+            OperatorConstants.LEFT_Y_DEADBAND),
+        () -> MathUtil.applyDeadband(driverController.getLeftX(),
+            OperatorConstants.LEFT_X_DEADBAND),
+        () -> {
+          double rightX = driverController.getRightX();
+          // System.out.println("RightX: " + rightX);
+          return rightX;
+        },
+        () -> {
+          double rightY = driverController.getRightY();
+          // System.out.println("RightY: " + rightY);
+          return rightY;
+        });
+
+    Command driveFieldOrientedAnglularVelocity = drive.driveCommand(
+        () -> MathUtil.applyDeadband(driverController.getLeftY(),
+            OperatorConstants.LEFT_Y_DEADBAND),
+        () -> MathUtil.applyDeadband(driverController.getLeftX(),
+            OperatorConstants.LEFT_X_DEADBAND),
+        () -> driverController.getRawAxis(2));
+
+    // drive.setDefaultCommand(driveFieldOrientedDirectAngle);
+    drive.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+
   }
 
   private void configureBindings() {
     // 6 -> right bumper on Xbox Controller
-    // new JoystickButton(driverController, 6).onTrue((new
-    // InstantCommand(drive::zeroGyro)));
+    new JoystickButton(driverController, 6).onTrue((new InstantCommand(drive::zeroGyro)));
 
     //// SmartDashboard controls
     // Collector
