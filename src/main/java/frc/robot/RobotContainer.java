3d3690dd6e38a794593ea272ded6759e2a719d03
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2024-02-24 13:07:41 -0500

    Swerve work

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,21 +38,60 @@ public class RobotContainer {
             OperatorConstants.LEFT_X_DEADBAND),
         () -> {
           double rightX = driverController.getRightX();
-          // System.out.println("RightX: " + rightX);
-          return -rightX;
+
+          SmartDashboard.putNumber("X", rightX);
+          return snapToRightAngle(-rightX);
         },
         () -> {
           double rightY = driverController.getRightY();
-          // System.out.println("RightY: " + rightY);
-          return -rightY;
+          SmartDashboard.putNumber("Y", rightY);
+          return snapToRightAngle(-rightY);
         });
 
     drive.setDefaultCommand(driveFieldOrientedDirectAngle);
   }
 
+  // 0.96 -> 1
+  // 0.99 -> 1
+  // 0.88 -> 1
+  // 0.60 -> 0.6
+  // 0.04 -> 0
+  // -0.04 -> 0
+
+  private double snapToRightAngle(double i) {
+    double snapThreshold = 0.2;
+
+    if (i < 0 + snapThreshold && i > 0 - snapThreshold) {
+      return 0;
+    } else if (i > 1 - snapThreshold) {
+      return 1;
+    } else if (i < snapThreshold - 1) {
+      return -1;
+    } else {
+      return i;
+    }
+  }
+
   private void configureBindings() {
+    configureDriverController();
+  }
+
+  private void configureDriverController() {
     // 6 -> right bumper on Xbox Controller
     new JoystickButton(driverController, 6).onTrue((new InstantCommand(drive::zeroGyro)));
+
+    // A -> swerve angle down (+ 1 is down)
+    /*
+     * new JoystickButton(driverController, 1)
+     * .onTrue(
+     * drive.driveCommand(
+     * () -> -MathUtil.applyDeadband(driverController.getLeftY(),
+     * OperatorConstants.LEFT_Y_DEADBAND),
+     * () -> -MathUtil.applyDeadband(driverController.getLeftX(),
+     * OperatorConstants.LEFT_X_DEADBAND),
+     * () -> 0,
+     * () -> 1));
+     */
   }
 
   public Command getAutonomousCommand() {
