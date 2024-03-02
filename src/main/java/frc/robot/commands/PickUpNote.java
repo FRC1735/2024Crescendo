f70f1db605d1f7a3600ad61a8f85d9b615c57472
green-reaper-1735 <45584689+green-reaper-1735@users.noreply.github.com>
2024-03-02 10:40:36 -0500

    Stop collector when PickUpNote ends

--- src/main/java/frc/robot/commands/PickUpNote.java
+++ src/main/java/frc/robot/commands/PickUpNote.java
@@ -31,6 +31,7 @@ public class PickUpNote extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    this.collector.stop();
   }
 
   // Returns true when the command should end.
